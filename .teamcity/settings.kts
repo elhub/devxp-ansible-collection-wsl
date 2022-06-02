fe3c61a9c878f923925f4cf4a8d3873a28c32da7
Michael Akinde <michael.akinde@statnett.no>
2022-06-02 11:09:43 +0200

    Add test teamcity config
    Test Plan: Run in sandbox on TC
    
    Reviewers: O5 elhub/devxp, sergei.prodanov
    
    Reviewed By: O5 elhub/devxp, sergei.prodanov
    
    Subscribers: sergei.prodanov
    
    JIRA Issues: TD-2567
    
    Differential Revision: https://phabricator.elhub.cloud/D6332

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -8,6 +8,10 @@ import no.elhub.common.build.configuration.CodeReview
 import no.elhub.common.build.configuration.ProjectType.ANSIBLE
 import no.elhub.common.build.configuration.SonarScan
 import no.elhub.common.build.configuration.UnitTest
+import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
+import jetbrains.buildServer.configs.kotlin.v2019_2.VcsRoot
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+
 
 version = "2022.4"
 
@@ -17,7 +21,7 @@ project {
     val projectType = ANSIBLE
 
     params {
-        param("teamcity.ui.settings.readOnly", "true")
+//        param("teamcity.ui.settings.readOnly", "true")
     }
 
     val sonarScanConfig = SonarScan.Config(
@@ -26,7 +30,105 @@ project {
         sonarId = projectId
     )
 
-    // Release Build Chain
-    listOf(sonarScan).forEach { buildType(it) }
+    val sonarScan = SonarScan(sonarScanConfig) {
+        vcs {
+            branchFilter = """
+                -:*
+                +:<default>
+            """.trimIndent()
+            cleanCheckout = true
+        }
+    }
 
+    val release = AutoRelease(
+        AutoRelease.Config(
+            vcsRoot = DslContext.settingsRoot,
+            type = projectType
+        )
+    ) {
+        vcs {
+            branchFilter = """
+                -:*
+                +:<default>
+            """.trimIndent()
+            cleanCheckout = true
+        }
+
+        dependencies {
+            snapshot(sonarScan) { }
+        }
+
+        triggers {
+            vcs {
+                branchFilter = """
+                    -:*
+                    +:<default>
+                """.trimIndent()
+
+                quietPeriodMode = VcsTrigger.QuietPeriodMode.USE_DEFAULT
+            }
+        }
+    }
+
+    listOf("Adr", "Ansible", "Arcanist", "Base", "Docker", "Git", "GitUtils", "Java", "Kotlin", "Linters", "Molecule",
+        "Node", "Python").forEach { role ->
+            buildType(MoleculeTest(MoleculeTest.Config(
+                DslContext.settingsRoot,
+                role
+            )))
+    }
+
+    // Release Build Chain
+    listOf(sonarScan, release).forEach { buildType(it) }
+
+    buildType(
+        CodeReview(
+            CodeReview.Config(
+                vcsRoot = DslContext.settingsRoot,
+                type = ANSIBLE,
+                sonarScanConfig = sonarScanConfig
+            )
+        )
+    )
+
+}
+
+class MoleculeTest(config: Config, buildConfig: BuildType.() -> Unit = {}) : BuildType({
+    id("MoleculeTest_${config.role}")
+    name = "Molecule Test ${config.role}"
+    description = "Run molecule tests for ${config.role}."
+
+    vcs {
+        root(config.vcsRoot)
+    }
+
+    steps {
+        script {
+            scriptContent = """
+                cd roles/${config.role}; molecule test
+            """.trimIndent()
+        }
+    }
+
+    triggers {
+        vcs {
+            branchFilter = "+:<default>"
+            quietPeriodMode = VcsTrigger.QuietPeriodMode.USE_DEFAULT
+            triggerRules = "+:roles/${config.role}/**"
+        }
+    }
+
+    apply(buildConfig)
+
+}) {
+    /**
+     * Config class for [MoleculeTest] build configuration
+     *
+     * @param vcsRoot              The VCS root
+     * @param role                 The role to be tested by this build configurations
+     */
+    data class Config(
+        val vcsRoot: VcsRoot,
+        val role: String
+    )
 }
