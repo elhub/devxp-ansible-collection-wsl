9ae99a252ae312b6642740e54acd0f435f2a282d
Johannes Schaanning <57237778+LonelyLlamas@users.noreply.github.com>
2024-01-02 13:35:26 +0100

    Update build configuration version (#7)
    * Update build configuration [patch]
    
    * update settings kts
    
    ---------
    
    Co-authored-by: johannes.schaanning <johannes.schaanning@statnett.no>

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,134 +1,25 @@
-import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
-import jetbrains.buildServer.configs.kotlin.v2019_2.DslContext
-import jetbrains.buildServer.configs.kotlin.v2019_2.VcsRoot
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
-import jetbrains.buildServer.configs.kotlin.v2019_2.project
-import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.VcsTrigger
-import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
-import jetbrains.buildServer.configs.kotlin.v2019_2.version
-import no.elhub.devxp.build.configuration.AutoRelease
-import no.elhub.devxp.build.configuration.CodeReview
-import no.elhub.devxp.build.configuration.ProjectType.ANSIBLE
-import no.elhub.devxp.build.configuration.SonarScan
+import no.elhub.devxp.build.configuration.pipeline.ElhubProject.Companion.elhubProject
+import no.elhub.devxp.build.configuration.pipeline.constants.Group.DEVXP
+import no.elhub.devxp.build.configuration.pipeline.jobs.ansibleAutoRelease
+import no.elhub.devxp.build.configuration.pipeline.jobs.ansibleSonarScan
+import no.elhub.devxp.build.configuration.pipeline.jobs.moleculeTest
 
-version = "2022.10"
+elhubProject(DEVXP, "ansible-collection-wsl") {
 
-project {
+    val roles = listOf("adr", "ansible", "arcanist", "base", "docker", "git", "gitutils", "java", "kotlin", "linters", "molecule",
+        "node", "python")
 
-    val projectId = "no.elhub.devxp:ansible-collection-wsl"
-    val projectType = ANSIBLE
-
-    params {
-        param("teamcity.ui.settings.readOnly", "true")
-    }
-
-    val sonarScanConfig = SonarScan.Config(
-        vcsRoot = DslContext.settingsRoot,
-        type = projectType,
-        sonarId = projectId
-    )
-
-    val sonarScan = SonarScan(sonarScanConfig) {
-        vcs {
-            branchFilter = """
-                -:*
-                +:<default>
-            """.trimIndent()
-            cleanCheckout = true
+    phabricator {
+        sequential {
+            ansibleSonarScan()
+            roles.forEach { moleculeTest(it) }
         }
     }
 
-    val release = AutoRelease(
-        AutoRelease.Config(
-            vcsRoot = DslContext.settingsRoot,
-            type = projectType
-        )
-    ) {
-        vcs {
-            branchFilter = """
-                -:*
-                +:<default>
-            """.trimIndent()
-            cleanCheckout = true
-        }
-
-        dependencies {
-            snapshot(sonarScan) { }
-        }
-
-        triggers {
-            vcs {
-                branchFilter = """
-                    -:*
-                    +:<default>
-                """.trimIndent()
-
-                quietPeriodMode = VcsTrigger.QuietPeriodMode.USE_DEFAULT
-            }
+    pipeline {
+        sequential {
+            roles.forEach { moleculeTest(it) }
+            ansibleAutoRelease()
         }
     }
-
-    listOf(
-        "Adr", "Ansible", "Arcanist", "Base", "Docker", "Git", "GitUtils", "Java", "Kotlin", "Linters", "Molecule",
-        "Node", "Python"
-    ).forEach { role ->
-        buildType(
-            MoleculeTest(
-                MoleculeTest.Config(DslContext.settingsRoot, role)
-            )
-        )
-    }
-
-    // Release Build Chain
-    listOf(sonarScan, release).forEach { buildType(it) }
-
-    buildType(
-        CodeReview(
-            CodeReview.Config(
-                vcsRoot = DslContext.settingsRoot,
-                type = ANSIBLE,
-                sonarScanConfig = sonarScanConfig
-            )
-        )
-    )
-}
-
-class MoleculeTest(config: Config, buildConfig: BuildType.() -> Unit = {}) : BuildType({
-    id("MoleculeTest_${config.role}")
-    name = "Molecule Test ${config.role}"
-    description = "Run molecule tests for ${config.role}."
-
-    vcs {
-        root(config.vcsRoot)
-    }
-
-    steps {
-        script {
-            scriptContent = """
-                cd roles/${config.role}; molecule test
-            """.trimIndent()
-        }
-    }
-
-    triggers {
-        vcs {
-            branchFilter = "+:<default>"
-            quietPeriodMode = VcsTrigger.QuietPeriodMode.USE_DEFAULT
-            triggerRules = "+:roles/${config.role}/**"
-        }
-    }
-
-    apply(buildConfig)
-
-}) {
-    /**
-     * Config class for [MoleculeTest] build configuration
-     *
-     * @param vcsRoot              The VCS root
-     * @param role                 The role to be tested by this build configurations
-     */
-    data class Config(
-        val vcsRoot: VcsRoot,
-        val role: String
-    )
 }
