d50ff47ee8fb7f328ca39449141c21a06ae807da
Brian Joseph Donohoe <brian.donohoe@statnett.no>
2024-09-12 10:35:24 +0200

    Bump pom and update setting.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -6,10 +6,10 @@ import no.elhub.devxp.build.configuration.pipeline.jobs.moleculeTest
 
 elhubProject(DEVXP, "devxp-ansible-collection-wsl") {
 
-    val roles = listOf("adr", "ansible", "arcanist", "base", "docker", "git", "gitutils", "java", "kotlin", "linters", "molecule",
+    val roles = listOf("adr", "ansible", "arcanist", "base", "docker", "git", "git_utils", "java", "kotlin", "linters", "molecule",
         "node", "python")
 
-    phabricator {
+    codeReview {
         sequential {
             ansibleSonarScan()
             parallel {
