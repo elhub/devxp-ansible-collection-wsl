f420800055e7b17c031fad2a8fc50dc64140f33a
Brian Joseph Donohoe <brian.donohoe@statnett.no>
2026-06-03 10:31:39 +0200

    Fix: Add retries for sdkman java install and fix molecule test

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -7,15 +7,16 @@ import no.elhub.devxp.build.configuration.pipeline.jobs.makeVerify
 elhubProject(DEVXP, "devxp-ansible-collection-wsl") {
 
     val roles = listOf(
-        "adr", "ansible", "arcanist", "base", "docker", "git", "git_utils", "java", "kotlin", "linters", "molecule",
-        "node", "python"
+        "java"
+        // "adr", "ansible", "arcanist", "base", "docker", "git", "git_utils", "kotlin", "linters", "molecule",
+        // "node", "python"
     )
 
     pipeline {
         sequential {
-//            parallel {
-//                roles.forEach { moleculeTest(it) }
-//            }
+            parallel {
+                roles.forEach { moleculeTest(it) }
+            }
             makeVerify {
                 sonarScanSettings = {
                     sonarProjectModules = roles.map { "roles/$it" }
